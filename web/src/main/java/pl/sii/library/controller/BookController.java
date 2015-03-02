1a8dd1f7c8eadc6b74d8fe7b6c1728e0207ae77c
tomasz-szymanski <szymaniaq@gmail.com>
2015-03-02 13:16:07 +0100

    adding ejb, hibernate, jpa and jsf

---
+++ web/src/main/java/pl/sii/library/controller/BookController.java
@@ -0,0 +1,88 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.sii.library.controller;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pl.sii.library.model.Book;
+import pl.sii.library.service.BookOperations;
+
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://www.cdi-spec.org/faq/#accordion6
+@Model
+public class BookController {
+
+    @Inject
+    private FacesContext facesContext;
+
+    @Inject
+    private BookOperations bookOperations;
+
+    private Book newBook;
+
+    @Produces
+    @Named
+    public Book getNewBook() {
+        return newBook;
+    }
+
+    public void createBook() {
+        try {
+            bookOperations.create(newBook);
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Book added!", "Book creation successful"));
+            initNewBook();
+        } catch (Exception e) {
+            String errorMessage = getRootErrorMessage(e);
+            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    errorMessage, "Book creation unsuccessful"));
+        }
+    }
+
+    @PostConstruct
+    public void initNewBook() {
+        newBook = new Book();
+    }
+
+    private String getRootErrorMessage(Exception e) {
+        // Default to general error message that registration failed.
+        String errorMessage = "Book creation failed. See server log for more information";
+        if (e == null) {
+            // This shouldn't happen, but return the default messages
+            return errorMessage;
+        }
+
+        // Start with the exception and recurse to find the root cause
+        Throwable t = e;
+        while (t != null) {
+            // Get the message from the Throwable class instance
+            errorMessage = t.getLocalizedMessage();
+            t = t.getCause();
+        }
+        // This is the root cause message
+        return errorMessage;
+    }
+
+}
